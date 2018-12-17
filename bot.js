7704280855e49ead81d49e9d6c01e7eb7007b1f1
MrAshrafa <45470917+MrAshrafa@users.noreply.github.com>
2018-12-17 15:02:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -68,40 +68,6 @@ client.on('message', async message => {
     }
 });
 
-client.on('message', msg => {
-  if (msg.author.bot) return;
-  if (!msg.content.startsWith(prefix)) return;
-  let command = msg.content.split(" ")[0];
-  command = command.slice(prefix.length);
-  let args = msg.content.split(" ").slice(1);
-
-    if(command === prefix + "clear") {
-        const emoji = client.emojis.find("name", "wastebasket")
-    let textxt = args.slice(0).join("");
-    if(msg.member.hasPermission("MANAGE_MESSAGES")) {
-    if (textxt == "") {
-        msg.delete().then
-    msg.delete().then
-    msg.delete().then
-client.on("message", message => {
-    var args = message.content.substring(prefix.length).split(" ");
-    if (message.content.startsWith(prefix + "clear")) { // 
-        if(!message.channel.guild) return message.reply('**❌ اسف لكن هذا الامر للسيرفرات فقط **');         
-if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**⚠  لا يوجد لديك صلاحية لمسح الشات**');
-var msg;
-msg = parseInt();
-
-message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
-message.channel.sendMessage("", {embed: {
-title: "```php\nعدد الرسائل التي تم مسحها: " + textxt + "\n```"
-color: 00055,
-footer: {
-  
-}
-}}).then(msg => {msg.delete(3000)});
-                  }
-
-});
 
 client.on('message', message => {
          if(message.content === prefix + "closeroom") {
