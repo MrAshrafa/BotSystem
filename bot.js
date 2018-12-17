88f0de5c67f921b70a4dcb0b498bf2f11ea1ff22
MrAshrafa <45470917+MrAshrafa@users.noreply.github.com>
2018-12-17 15:38:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,7 +22,7 @@ client.on('message',async message => {
     
 if(message.author.bot) return;
 if(!credits[message.author.id]) credits[message.author.id] = {
-    credits: 50
+    credits: 100
 };
 
 let userData = credits[message.author.id];
@@ -43,7 +43,7 @@ message.channel.send(`**${message.author.username}, your :credit_card: balance i
 });
 
 client.on('message', async message => {
-    let amount = 1000000000;
+    let amount = 300;
     if(message.content.startsWith(prefix + "daily")) {
     if(message.author.bot) return;
     if(coolDown.has(message.author.id)) return message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes in \`\`1 Day\`\`.**`);
